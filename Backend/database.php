23f85d22aa902d4f7f40b4d0a57512e51282cb91
HyperNite <77816917+HyperNIt3@users.noreply.github.com>
2022-01-27 16:37:49 -0600

    Add files via upload

---
+++ Backend/database.php
@@ -0,0 +1,16 @@
+<?php $dataBase = array (
+  0 => 
+  array (
+    'Key' => 'LostX-fakeKey',
+    'Discord' => 'Discord ID',
+    'dateWhitelisted' => '10:39 PM, 04/10/2020',
+    'isDeveloper' => true,
+    'isFrozen' => false,
+    'hasBeenSuspended' => false,
+    'suspensions' => '',
+    'isBlacklisted' => false,
+    'ip' => '',
+    'HWID' => 'HWID HERE',
+    'Exploit' => 'syn',
+    'lastHwidChange' => '2020/05/29',
+  ),); ?>
